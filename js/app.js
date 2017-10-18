96e99453a34098e73ac4f4184cd1aa4b29393165
Rafael Heringer <rafaheringer@live.com>
2017-10-18 15:34:39 -0200

    Adicionando vagas

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
-(function(){
+(($) => {
     //Typing effect
-    var typed = new Typed($('#dinamic-type').text('').get(0), {
+    new Typed($('#dinamic-type').text('').get(0), {
         strings: ['.NET CORE?', 'NoSQL?', 'gente boa?', 'autonomia?', 'squads?', 'flexibilidade?'],
         typeSpeed: 140,
         loop: true,
         backDelay: 1500,
         cursorChar: '_'
     });
-})();
+
+    //Opened jobs
+    $('.section-open-jobs-menu-item','.section-open-jobs-menu').each(function() {
+        $(this).on('click', function(event) {
+            $(
+                $(this)
+                .addClass('section-open-jobs-menu-item-selected')
+                .siblings()
+                .removeClass('section-open-jobs-menu-item-selected')
+                .end()
+                .find('.section-open-jobs-menu-item-link').attr('href')
+            )
+            .show().siblings().hide();
+
+            event.preventDefault();
+        });
+    }).first().trigger('click');
+})(jQuery);
